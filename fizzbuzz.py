f95ef6c3662c3440a78af248c9880d931734628b
DanMeltz <dan.meltz@controlgroup.com>
2014-09-04 21:16:22 -0400

    fixed the comments. again.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -15,13 +15,13 @@ first_replace = 'Fizz'
 second_replace = 'Buzz'
 
 
-for count in range(start_no , end_no + 1):         # "for" will only go up to the the last number minus one, hence the "+ 1"
+for count in range(start_no , end_no + 1):               # "for" will only go up to the the last number minus one, hence the "+ 1"
 	if (count % (first_find * second_find)) == 0:    # Check for both first, otherwise the other two will prevent it from ever getting hit
 		print first_replace, second_replace            		
 	elif (count % first_find) == 0:                  # Then look for the first target 
-		print first_replace                            # and print the replacement instead of the number
+		print first_replace                      # and print the replacement instead of the number
 	elif (count % second_find) == 0:                 # IBID
 		print second_replace
 	else:
-		print(count)                                   # If none of the replacements hit, just print a boring old number. Yawn.
+		print(count)                             # If none of the replacements hit, just print a boring old number. Yawn.
 
